49c30109d621c7bf073dcda19fc7ab4127e3ca82
wanghao <wanghao@NickMacBook-Pro.local>
2016-03-10 20:58:56 +0800

    抢单展示

--- AutoBon/app/src/main/java/cn/com/incardata/autobon/MainAuthorizedActivity.java
+++ AutoBon/app/src/main/java/cn/com/incardata/autobon/MainAuthorizedActivity.java
@@ -1,13 +1,28 @@
 package cn.com.incardata.autobon;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
+import com.alibaba.fastjson.JSON;
+
+import cn.com.incardata.fragment.IndentMapFragment;
+import cn.com.incardata.getui.ActionType;
+import cn.com.incardata.getui.CustomIntentFilter;
+import cn.com.incardata.getui.OrderMsg;
 
 /**
  * 已认证的
  * @author wanghao
  */
-public class MainAuthorizedActivity extends BaseActivity implements View.OnClickListener{
+public class MainAuthorizedActivity extends BaseActivity implements View.OnClickListener, IndentMapFragment.OnFragmentInteractionListener{
+    private FragmentManager fragmentManager;
+    private IndentMapFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,11 @@ public class MainAuthorizedActivity extends BaseActivity implements View.OnClick
 
     private void init() {
         findViewById(R.id.personal).setOnClickListener(this);
+
+        fragmentManager = getFragmentManager();
+        mFragment = new IndentMapFragment();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.order_container, mFragment).commit();
     }
 
     @Override
@@ -27,5 +47,37 @@ public class MainAuthorizedActivity extends BaseActivity implements View.OnClick
                 startActivity(MyInfoActivity.class);
                 break;
         }
+    }
+
+    private final BroadcastReceiver mOrderReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (ActionType.ACTION_ORDER.equals(action)){
+                if (mFragment != null){
+                    String json = intent.getStringExtra(ActionType.NEW_ORDER);
+                    OrderMsg orderMsg = JSON.parseObject(json, OrderMsg.class);
+                    mFragment.setData(orderMsg.getOrder());
+                }
+            }
+
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mOrderReceiver, CustomIntentFilter.getOrderIntentFilter());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mOrderReceiver);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
